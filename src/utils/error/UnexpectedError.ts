7eb3296425496559aadc6c86a62bbff6b7ce58a6
meduduna Akerejola <cremmybanj7@gmail.com>
2022-07-13 15:08:20 +0100

    nice and simple

--- src/utils/error/UnexpectedError.ts
+++
@@ -1,8 +0,0 @@
-import { API_ERRORS } from '../../types/app.errors';
-import { ApiError } from './APIError';
-
-export class UnexpectedError extends ApiError {
-    constructor() {
-        super(API_ERRORS.GENERAL_ERROR);
-    }
-}
